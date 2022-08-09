14c230b4c2caad13e6eb367352446164cfaec81a
mjjh1717git <1692762315@qq.com>
2022-08-09 21:36:29 +0800

    '路由鉴权'

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,17 +2,25 @@ import App from "../App.jsx";
 import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
 import Notfound from "@/pages/Notfound";
+
+import { AuthRoute } from "@/components/AuthRoute";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //定义一个路由
 const BaseRouter = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="/layout" element={<Layout />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/*" element={<Notfound />}></Route>
-      </Route>
+      <Route
+        path="/"
+        element={
+          <AuthRoute>
+            <Layout />
+          </AuthRoute>
+        }
+      ></Route>
+      <Route path="/layout" element={<Layout />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/*" element={<Notfound />}></Route>
     </Routes>
   </BrowserRouter>
 );
